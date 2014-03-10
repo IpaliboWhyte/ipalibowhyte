7351db41612720856ee8a386dcb37ddb7180a2a3
Ipalibo <ipalibowhyte@gmail.com>
2014-03-10 21:23:25 +0000

    Finished Basic Layout
    -Not Responsive Yet

---
+++ scripts/Pagejavascript.js
@@ -0,0 +1,50 @@
+function setbackground(){
+
+var randomColor;
+
+var bgcolorlist=new Array("152,197,253", "137,233,141", "252,83,103", "152,92,180", "255,190,38");
+
+//document.body.style.background=bgcolorlist[Math.floor(Math.random()*bgcolorlist.length)];
+
+randomColor = bgcolorlist[Math.floor(Math.random()*bgcolorlist.length)];
+
+document.body.style.backgroundColor= 'rgb('+randomColor+')';
+
+}
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({scrollTop: target.offset().top}, 2000);
+        return false;
+      }
+    }
+  });
+});
+
+
+$(function() {
+    //  changes mouse cursor when highlighting loawer right of box
+    $("textarea").mousemove(function(e) {
+        var myPos = $(this).offset();
+        myPos.bottom = $(this).offset().top + $(this).outerHeight();
+        myPos.right = $(this).offset().left + $(this).outerWidth();
+        
+        if (myPos.bottom > e.pageY && e.pageY > myPos.bottom - 16 && myPos.right > e.pageX && e.pageX > myPos.right - 16) {
+            $(this).css({ cursor: "nw-resize" });
+        }
+        else {
+            $(this).css({ cursor: "" });
+        }
+    })
+    //  the following simple make the textbox "Auto-Expand" as it is typed in
+    .keyup(function(e) {
+        //  the following will help the text expand as typing takes place
+        while($(this).outerHeight() < this.scrollHeight + parseFloat($(this).css("borderTopWidth")) + parseFloat($(this).css("borderBottomWidth"))) {
+            $(this).height($(this).height()+1);
+        };
+    });
+});
